2d54dd58a2e28810ba75ca7b769726c7f2deebd4
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-04-25 19:46:19 -0300

    Add produtos array with food items and prices

--- front-end/dados.js
+++ front-end/dados.js
@@ -1,1 +1,6 @@
-
+// dados.js
+export const produtos = [
+    { id: 1, nome: "Tteokbokki", preco: 27.16, foto: "https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2023/07/28/1199320667-kimchi-coreano.jpg" },
+    { id: 2, nome: "Buchimgae", preco: 10.98, foto: "https://static.itdg.com.br/repository/recipe/video/17/75/1775.jpg" },
+    { id: 3, nome: "Lámen Especial", preco: 9.99, foto: "https://m.media-amazon.com/images/I/51pS9k+O7KL._AC_SX679_.jpg" }
+];
