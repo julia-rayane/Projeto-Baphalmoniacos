fd959ee86941110b84e26e02a70b675d560330a7
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-10 13:41:35 -0300

    Update routes.js

--- projeto-backend/src/routes.js
+++ projeto-backend/src/routes.js
@@ -1,1 +1,14 @@
+import express from 'express';
+import { produtos } from './data.js'; // Importa os dados que acabamos de criar
 
+const routes = express.Router();
+
+// A rota de listagem de produtos agora mora aqui
+routes.get('/produtos', (req, res) => {
+    res.json(produtos);
+});
+
+// Aqui você pode adicionar as outras rotas do CRUD (Post, Put, Delete) depois
+// routes.post('/produtos', (req, res) => { ... });
+
+export default routes;
