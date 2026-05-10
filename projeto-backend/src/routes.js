c797624e53ea4d92fc2582ae55a82ae3e050b1ab
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-10 14:15:21 -0300

    Update routes.js

--- projeto-backend/src/routes.js
+++ projeto-backend/src/routes.js
@@ -1,14 +1,28 @@
 import express from 'express';
-import { produtos } from './data.js'; // Importa os dados que acabamos de criar
+import { produtos } from './data.js';
 
 const routes = express.Router();
 
-// A rota de listagem de produtos agora mora aqui
+// Rota de Leitura (que você já tinha)
 routes.get('/produtos', (req, res) => {
     res.json(produtos);
 });
 
-// Aqui você pode adicionar as outras rotas do CRUD (Post, Put, Delete) depois
-// routes.post('/produtos', (req, res) => { ... });
+// --- O CRUD COMPLETO (SÓ AS ROTAS POR ENQUANTO) ---
 
-export default routes;
+// Rota de Criação (POST)
+routes.post('/produtos', (req, res) => { 
+    res.send('Aqui você criaria um produto'); 
+});
+
+// Rota de Atualização (PUT)
+routes.put('/produtos/:id', (req, res) => { 
+    res.send('Aqui você editaria um produto'); 
+});
+
+// Rota de Exclusão (DELETE)
+routes.delete('/produtos/:id', (req, res) => { 
+    res.send('Aqui você deletaria um produto'); 
+});
+
+export default routes; // <--- Essa linha tem que ser sempre a última!
