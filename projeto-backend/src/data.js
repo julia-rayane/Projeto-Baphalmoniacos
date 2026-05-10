ad5478e47a521b84dca23883069a0cb48aa9a6dc
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-10 13:39:01 -0300

    Add initial product data to data.js

---
+++ projeto-backend/src/data.js
@@ -0,0 +1,21 @@
+// Aqui ficam os seus dados (Ingredientes da cozinha)
+export const produtos = [
+    { 
+        id: 1, 
+        nome: "Tteokbokki", 
+        preco: 27.16, 
+        foto: "https://images.unsplash.com/photo-1585032226651-759b368d7246?q=80&w=500" // Troquei o link local por um da web para não dar erro
+    },
+    { 
+        id: 2, 
+        nome: "Buchimgae", 
+        preco: 10.98, 
+        foto: "https://www.thespruceeats.com/thmb/9W6-7L20lYp6f9yU8lX7r9m1_6o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/korean-pancake-pajeon-2118742-Hero-5b72e9a246e0fb00259e8f6e.jpg" 
+    },
+    { 
+        id: 3, 
+        nome: "Lámen Especial", 
+        preco: 9.99, 
+        foto: "https://cdn.casaogawa.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/o/nongshim_shin_ramyun_cup_68g.jpg" 
+    }
+];
