3f99b4244f3052cebc14d9b92019c2a29188f6e1
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-31 19:52:50 -0300

    Update index.js

--- projeto-backend/src/index.js
+++ projeto-backend/src/index.js
@@ -4,9 +4,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import routes from './routes.js';
+// Importa o router que exportamos com chaves { router } no routes.js
+import { router } from './routes.js';
 
-import { conectarBanco } from './database/connection.js';
+//puxa do arquivo novo 'database.js'
+import { conectarBanco } from './database/database.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,25 +16,23 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-
-app.use(routes);
-
+// Usa a variável router que importou lá em cima
+app.use(router);
 
 async function startServer() {
   try {
-    
-    const db = await conectarBanco();
+    // Testa a conexão com o banco de dados antes de abrir o servidor
+    await conectarBanco();
     console.log('📦 Banco de dados SQLite conectado com sucesso!');
 
     app.listen(PORT, () => {
-      console.log('🚀 Servidor rodando em http://localhost:3000');
+      console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error('Erro ao inicializar o projeto:', error);
