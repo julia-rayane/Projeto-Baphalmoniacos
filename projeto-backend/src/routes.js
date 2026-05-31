07d15f7732be36c355cc9239deba2f2a09d3424e
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-31 19:34:13 -0300

    Update routes.js

--- projeto-backend/src/routes.js
+++ projeto-backend/src/routes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
-// Importa todas as funções lógicas que você acabou de criar no Controller
+//aponta para a pasta controllers
 import * as ProductController from './controllers/productController.js';
 
-const routes = Router();
+const router = Router();
 
 // Mapeamento das rotas do CRUD de produtos 
-routes.get('/produtos', ProductController.listProducts);
-routes.get('/produtos/:id', ProductController.showProduct);
-routes.post('/produtos', ProductController.addProduct);
-routes.put('/produtos/:id', ProductController.editProduct);
-routes.delete('/produtos/:id', ProductController.removeProduct);
+router.get('/produtos', ProductController.listProducts);
+router.get('/produtos/:id', ProductController.showProduct);
+router.post('/produtos', ProductController.addProduct);
+router.put('/produtos/:id', ProductController.editProduct);
+router.delete('/produtos/:id', ProductController.removeProduct);
 
-export default routes;
+export { router };
