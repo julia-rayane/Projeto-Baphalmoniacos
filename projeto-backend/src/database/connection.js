79084057053344b350f1aa8fed9c1fa51263a9a9
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-05-31 14:41:00 -0300

    Update connection.js

--- projeto-backend/src/database/connection.js
+++ projeto-backend/src/database/connection.js
@@ -1,11 +1,15 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Função padrão que todo o seu projeto (Model, Migrations, Seeds) vai usar
 export async function conectarBanco() {
-    const db = await open({
-        filename: "./src/database/baphalmoniacos.db",
-        driver: sqlite3.Database
-    });
-
-    return db;
+  return open({
+    filename: path.join(__dirname, 'baphalmoniacos.db'),
+    driver: sqlite3.Database
+  });
 }
