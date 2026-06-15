d848449a812cbbddfc67ef80fce1402c6b8211c0
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-06-15 11:32:09 -0300

    Update categoriaRoutes.ts

--- projeto-backend/src/routes/categoriaRoutes.ts
+++ projeto-backend/src/routes/categoriaRoutes.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import { 
+  listarCategorias, 
+  buscarCategoria, 
+  criarCategoria, 
+  removerCategoria 
+} from '../controllers/categoriaController.js';
 
 const router = Router();
 
-router.get('/');
+router.get('/', listarCategorias);
+router.get('/:id', buscarCategoria);
+router.post('/', criarCategoria);
+router.delete('/:id', removerCategoria);
 
 export default router;
