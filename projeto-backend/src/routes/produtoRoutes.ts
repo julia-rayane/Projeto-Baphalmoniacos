f8f643b8198040363ab6acca3aefc399ee61bcf2
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-06-15 10:45:50 -0300

    Create produtoRoutes.ts

---
+++ projeto-backend/src/routes/produtoRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+// Importaremos os controllers aqui na próxima etapa!
+
+const router = Router();
+
+// As definições de rotas vão entrar aqui assim que migrarmos os controllers
+router.get('/'); 
+
+export default router;
