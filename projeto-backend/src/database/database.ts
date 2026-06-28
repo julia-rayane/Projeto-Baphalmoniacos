00d3fad2843f190f1a468f912ddd2ef5c4a9d6bb
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-06-28 18:03:43 -0300

    Update database.ts

--- projeto-backend/src/database/database.ts
+++ projeto-backend/src/database/database.ts
@@ -1,37 +1,7 @@
-import sqlite3 from 'sqlite3';
-import { open, Database } from 'sqlite';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { PrismaClient } from '@prisma/client';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // Isso faz as buscas aparecerem no terminal 
+});
 
-let dbInstance: Database | null = null;
-
-export async function obterConexao(): Promise<Database> {
-  if (!dbInstance) {
-    dbInstance = await open({
-      filename: path.join(__dirname, 'baphalmoniacos.db'), 
-      driver: sqlite3.Database
-    });
-
-    // 🚀 TABELAS ATUALIZADAS COM A COLUNA FOTO!
-    await dbInstance.exec(`
-      CREATE TABLE IF NOT EXISTS categorias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL
-      );
-
-      CREATE TABLE IF NOT EXISTS produtos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        preco REAL NOT NULL,
-        descricao TEXT,
-        foto TEXT,
-        id_categoria_fk INTEGER,
-        FOREIGN KEY (id_categoria_fk) REFERENCES categorias(id)
-      );
-    `);
-    console.log('✅ Banco de dados inicializado e tabelas verificadas!');
-  }
-  return dbInstance;
-}
+export default prisma;
