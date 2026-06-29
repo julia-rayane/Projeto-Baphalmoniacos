b338a9a3c41a6d1510b49f2587d0b829b088c80d
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-06-28 23:43:02 -0300

    Update index.ts

--- projeto-backend/src/index.ts
+++ projeto-backend/src/index.ts
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // 🛡️ Middleware para exigir Content-Type: application/json em POST e PUT
-app.use(exigirJson);
+//app.use(exigirJson);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
