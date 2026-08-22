c778dd1bc19fc13439ad6fe3e6fb40922917ef77
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-08-22 17:49:58 -0300

    Protect product routes with authentication middleware
    Added authentication middleware to protected routes for creating, updating, and deleting products.

--- projeto-backend/src/routes/produtoRoutes.ts
+++ projeto-backend/src/routes/produtoRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import productController from '../controllers/productController.js';
+import { autenticacaoMiddleware } from '../middlewares/autenticacaoMiddleware.js';
 
 const router = Router();
 
+// Rotas públicas (qualquer um pode visualizar)
 router.get('/', productController.obterProdutos);
 router.get('/:id', productController.obterProdutoPorId);
-router.post('/', productController.criarProduto);
-router.put('/:id', productController.atualizarProduto);
-router.delete('/:id', productController.deletarProduto);
+
+// Rotas protegidas (exigem o Token JWT no cabeçalho Authorization)
+router.post('/', autenticacaoMiddleware, productController.criarProduto);
+router.put('/:id', autenticacaoMiddleware, productController.atualizarProduto);
+router.delete('/:id', autenticacaoMiddleware, productController.deletarProduto);
 
 export default router;
