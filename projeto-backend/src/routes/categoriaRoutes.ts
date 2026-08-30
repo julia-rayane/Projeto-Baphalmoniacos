f048a945945af71f07c8b15bf3b85f95efc2cc08
julia-rayane <julia.rayane@academico.ifpb.edu.br>
2026-08-30 15:10:16 -0300

    Update categoriaRoutes.ts

--- projeto-backend/src/routes/categoriaRoutes.ts
+++ projeto-backend/src/routes/categoriaRoutes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import categoriaController from '../controllers/categoriaController.js';
 import { autenticacaoMiddleware } from '../middlewares/autenticacaoMiddleware.js';
+import { apenasAdminMiddleware } from '../middlewares/apenasAdminMiddleware.js';
 
 const router = Router();
 
 // Rota pública (qualquer um pode listar)
 router.get('/', categoriaController.obterCategorias);
 
-// Rotas protegidas (exigem Token JWT)
-router.post('/', autenticacaoMiddleware, categoriaController.criarCategoria);
-router.put('/:id', autenticacaoMiddleware, categoriaController.atualizarCategoria);
-router.delete('/:id', autenticacaoMiddleware, categoriaController.deletarCategoria);
+// Rotas protegidas (exigem Token JWT válido E usuário do tipo admin)
+router.post('/', autenticacaoMiddleware, apenasAdminMiddleware, categoriaController.criarCategoria);
+router.put('/:id', autenticacaoMiddleware, apenasAdminMiddleware, categoriaController.atualizarCategoria);
+router.delete('/:id', autenticacaoMiddleware, apenasAdminMiddleware, categoriaController.deletarCategoria);
 
 export default router;
